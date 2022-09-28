7ac63317589d094e80e9a0fa6fa24916da15be0f
Stanislaw Glogowski <stanislaw.glogowski@gmail.com>
2022-09-28 22:09:14 +0200

    Release v2

--- dist/contracts.js
+++ dist/contracts.js
@@ -4,35 +4,35 @@ module.exports = {
   "ERC20Helper": {
     "addresses": {
       "56": "0x9780AF37513ffd6769B3Fdbc143071947Fa0Ab16",
-      "97": "0xe8a6f1926151E338b55d3eb6cEC6ce24BBa17496"
+      "97": "0xB25624dc3239B678eED4262d310Facc32d13E3e6"
     }
   },
   "MetaheroLoyaltyToken": {
     "addresses": {
-      "97": "0xc42b815de5e485A619313b696Ecb0C12cc4Cc7f2"
+      "97": "0x45Da7E96d4b0C26E053358658A54fd61d7c9A0c2"
     }
   },
   "MetaheroLoyaltyTokenDistributor": {
     "addresses": {
-      "97": "0x9DEF07b8aa8181b0C8a3e5E17DA8F5E689e5bCf8"
+      "97": "0xDe6AB34145cf170D0b1bB6C03562928BABb32EB7"
     }
   },
   "MetaheroDAO": {
     "addresses": {
       "56": "0x8369Feb2198D01a2ed5d78E89ca7507983028B42",
-      "97": "0xF21D52737e2c5Be950AB73edf4173619C491afE5"
+      "97": "0x7EEFC3E2dcb85c5Bd3E1142CD1D670286E25ca42"
     }
   },
   "MetaheroLPMForUniswapV2": {
     "addresses": {
       "56": "0x5DA5b71B88C42544b642D4AB781B30831EDAC341",
-      "97": "0x0b738d1B124f5a1082F155B92A7e6F5b49Ab8B03"
+      "97": "0x83eB30E0257718B0146583cBF0d0c0525B653939"
     }
   },
   "MetaheroToken": {
     "addresses": {
       "56": "0xD40bEDb44C081D2935eebA6eF5a3c8A31A1bBE13",
-      "97": "0x4bdf21169a6f8a069c210CF5868dD7a9cF65349A"
+      "97": "0xEbB1297f75F7Fd65A6c315F3A02cED5Ab08717eE"
     }
   },
   "BUSDToken": {
